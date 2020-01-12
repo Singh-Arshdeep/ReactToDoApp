a05efae31f2536204bd96f7fb7cd67d1f4bcbcc8
Arsh <arshdeepgary@gmail.com>
2020-01-11 19:29:58 -0800

    react forms

--- src/AppFetch.js
+++ src/AppFetch.js
@@ -5,8 +5,12 @@ class AppFetch extends React.Component {
     constructor() {
         super();
         this.state={
-            data:{}
+            data:{},
+            fname:'',
+            lname:''
         }
+        this.handleChange =  this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentDidMount() {
@@ -21,9 +25,31 @@ class AppFetch extends React.Component {
         );
     }
 
+    handleChange(event) {
+        this.setState(
+            {
+                [event.target.name]:event.target.value
+            }
+        );
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + event.target.value);
+        event.preventDefault();
+    }
+
     render() {
         return(
-        <h2>Hello world  {this.state.data.name}</h2>
+        <div>
+
+            <h2>Hello world  {this.state.data.name}</h2>
+            <form onSubmit={this.handleSubmit}>
+             <input type="text" name='fname' value={this.state.fname} onChange={this.handleChange} />
+             <input type="text" name='lname' value={this.state.lname} onChange={this.handleChange} />
+             <input type="submit" value="Submit" />
+             </form>
+            {this.state.fname} {this.state.lname}
+        </div>
         );
     }
 }
