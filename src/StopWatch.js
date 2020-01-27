27876da09bc5e16645ec70f2f5e9ed203bf09e1b
Arsh <arshdeepgary@gmail.com>
2020-01-26 23:17:53 -0800

    stop watch

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -2,16 +2,26 @@ import React, { useState,useEffect } from 'react';
 
 export default function StopWatch() {
     const [sec, setSec] = useState(0);
+    const [min, setMin] = useState(0);
 
     let handleClick = ()=>{
         setSec(prevTime => prevTime + 1);
         setTimeout(handleClick, 1000);
     }
 
+    useEffect(()=>{
+        console.log(sec);
+        if(sec==11) {
+            setSec(0);
+            setMin(prev=>prev+1);
+        }
+
+    });
+
     return(
         <div>
             <input type="button" value="click me" onClick={handleClick}/>
-            {sec}
+        {min}:{sec}
         </div>
     );
 }
