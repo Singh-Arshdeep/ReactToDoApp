ba8d59b8ea355658ccb8009b1eb3008167204c02
Arsh <arshdeepgary@gmail.com>
2020-01-31 22:08:55 -0800

    playing with stop watch

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -3,14 +3,24 @@ import React, { useState,useEffect } from 'react';
 export default function StopWatch() {
     const [sec, setSec] = useState(0);
     const [min, setMin] = useState(0);
+    const [lap, setLap] = useState(0);
 
     let handleClick = ()=>{
         setSec(prevTime => prevTime + 1);
         setTimeout(handleClick, 1000);
     }
 
+    let lapArr = [];
+    let handleLap = ()=> {
+        setLap(prev => prev + 1);
+        lapArr.push({
+            "sec":{sec},
+            "min":{min}
+        });
+        console.log(lapArr);
+    }
+
     useEffect(()=>{
-        console.log(sec);
         if(sec==11) {
             setSec(0);
             setMin(prev=>prev+1);
@@ -21,7 +31,14 @@ export default function StopWatch() {
     return(
         <div>
             <input type="button" value="click me" onClick={handleClick}/>
-        {min}:{sec}
+            <hr/>
+            {min}:{sec}
+            <hr/>
+            <input type="button" value="Lap" onClick={handleLap}/>
+            <hr/>
+            {lap}
+            <hr/>
+            {lapArr.sec}
         </div>
     );
 }
